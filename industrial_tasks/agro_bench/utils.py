45ad0593a65f29cf37a893fdfe2823fc6b77c73e
artemorloff <arorlov97@gmail.com>
2025-10-02 23:12:36 +0300

    update task filters

--- industrial_tasks/agro_bench/utils.py
+++ industrial_tasks/agro_bench/utils.py
@@ -1,11 +1,13 @@
 from typing import Dict, List, Any
-
+from lm_eval.api.filter import Filter
+from lm_eval.api.registry import register_filter
 from transformers.data.metrics import squad_metrics
 
 
 def doc_to_text(doc: Dict[str, Any]) -> str:
 
     return doc["instruction"].format(**dict(**doc["inputs"], **doc["meta"]))
+
 
 def process_results(doc: Dict, results: List[str]) -> Dict:
     if len(doc["outputs"]) > 0:
@@ -17,3 +19,22 @@ def process_results(doc: Dict, results: List[str]) -> Dict:
 
         return {"f1": f1, "em": em}
     return {"f1": 0, "em": 0}  # if no label provided (test answers are secret)
+
+
+@register_filter("remove_whitespace_and_nones")
+class RemoveWhitespaceAndNones(Filter):
+
+    def apply(self, resps: list[list[str]], docs: list[dict]) -> list[list[str]]:
+        def filter_set(inst):
+            filtered_resp = []
+            for resp in inst:
+                if not resp:
+                    resp = ""
+                else:
+                    resp = resp.lstrip()
+                filtered_resp.append(resp)
+            return filtered_resp
+
+        filtered_resps = [filter_set(resp) for resp in resps]
+
+        return filtered_resps
--- industrial_tasks/aqua_bench/utils.py
+++ industrial_tasks/aqua_bench/utils.py
@@ -1,5 +1,6 @@
 from typing import Dict, List, Any
-
+from lm_eval.api.filter import Filter
+from lm_eval.api.registry import register_filter
 from transformers.data.metrics import squad_metrics
 
 
@@ -18,3 +19,22 @@ def process_results(doc: Dict, results: List[str]) -> Dict:
 
         return {"f1": f1, "em": em}
     return {"f1": 0, "em": 0}  # if no label provided (test answers are secret)
+
+
+@register_filter("remove_whitespace_and_nones")
+class RemoveWhitespaceAndNones(Filter):
+
+    def apply(self, resps: list[list[str]], docs: list[dict]) -> list[list[str]]:
+        def filter_set(inst):
+            filtered_resp = []
+            for resp in inst:
+                if not resp:
+                    resp = ""
+                else:
+                    resp = resp.lstrip()
+                filtered_resp.append(resp)
+            return filtered_resp
+
+        filtered_resps = [filter_set(resp) for resp in resps]
+
+        return filtered_resps
