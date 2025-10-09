a92905de798023758bfa5afaeae05af827768a79
artemorloff <arorlov97@gmail.com>
2025-10-09 14:11:37 +0300

    remove unused scripts

--- run_log_to_submit.sh
+++
@@ -1,4 +0,0 @@
-python scripts/log_to_submission.py \
-  --outputs_dir ./test/Qwen__Qwen2.5-0.5B-Instruct \
-  --dst_dir submission_zip \
-  --model_args "pretrained=Qwen/Qwen2.5-0.5B-Instruct,dtype=bfloat16,tensor_parallel_size=1"
