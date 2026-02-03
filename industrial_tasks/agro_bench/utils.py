aa25e1e6ad65aa85a7aa16215bc090bc70fa972f
artemorlov <arorlov97@gmail.com>
2026-02-03 16:53:16 +0300

    fix bugs for newer codebase

--- industrial_tasks/agro_bench/utils.py
+++ industrial_tasks/agro_bench/utils.py
@@ -2,6 +2,8 @@ from typing import Dict, List, Any
 from lm_eval.api.filter import Filter
 from lm_eval.api.registry import register_filter
 from transformers.data.metrics import squad_metrics
+from lm_eval.api.registry import FILTER_REGISTRY
+
 
 
 def doc_to_text(doc: Dict[str, Any]) -> str:
@@ -21,20 +23,21 @@ def process_results(doc: Dict, results: List[str]) -> Dict:
     return {"f1": 0, "em": 0}  # if no label provided (test answers are secret)
 
 
-@register_filter("remove_whitespace_and_nones")
-class RemoveWhitespaceAndNones(Filter):
+if not FILTER_REGISTRY.get("remove_whitespace_and_nones", None):
+    @register_filter("remove_whitespace_and_nones")
+    class RemoveWhitespaceAndNones(Filter):
 
-    def apply(self, resps: list[list[str]], docs: list[dict]) -> list[list[str]]:
-        def filter_set(inst):
-            filtered_resp = []
-            for resp in inst:
-                if not resp:
-                    resp = ""
-                else:
-                    resp = resp.lstrip()
-                filtered_resp.append(resp)
-            return filtered_resp
+        def apply(self, resps: list[list[str]], docs: list[dict]) -> list[list[str]]:
+            def filter_set(inst):
+                filtered_resp = []
+                for resp in inst:
+                    if not resp:
+                        resp = ""
+                    else:
+                        resp = resp.lstrip()
+                    filtered_resp.append(resp)
+                return filtered_resp
 
-        filtered_resps = [filter_set(resp) for resp in resps]
+            filtered_resps = [filter_set(resp) for resp in resps]
 
-        return filtered_resps
+            return filtered_resps
